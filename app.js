ecddda41872f59fe73f7f5f02e90d9a44746524a
brob-dbod <barton@brobison.com>
2024-09-09 16:03:14 -0600

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World - this is Barton again - finally does it reflect my changes? YES!!! Now let's see it in the logs2.'));
+app.get('/', (req, res) => res.send('Hello World - this is Barton again - finally does it reflect my changes? YES!!! Now let us see it in the logs2.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
